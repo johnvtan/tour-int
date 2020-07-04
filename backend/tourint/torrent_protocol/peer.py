6adda14edc8ab888f9bde3860810b0d18568a96c
John Vincent Tan <john.v.tan@gmail.com>
2020-07-03 18:33:20 -0700

    WIP can connect with refactored version

--- backend/tourint/torrent_protocol/peer.py
+++ backend/tourint/torrent_protocol/peer.py
@@ -10,9 +10,11 @@ import hashlib
 if __package__ is None or __package__ == '':
     import consts
     import tracker
+    import ring_buffer
 else:
     from . import consts
     from . import tracker
+    from . import ring_buffer
 
 def read_from_socket_checked(s: socket.socket, size_bytes: int) -> bytes:
     ret = bytearray()
@@ -95,7 +97,10 @@ class PeerHandshake:
 
         return cls(peer_id, info_hash)
 
+
 class PeerMessage:
+    MESSAGE_LENGTH_SIZE: int = 4
+
     class Id(enum.Enum):
         # len == 1
         CHOKE = 0
@@ -114,34 +119,14 @@ class PeerMessage:
         # len == 0
         KEEP_ALIVE = 10
 
-    MESSAGE_LENGTH_SIZE: int = 4
+
     def __init__(self, message_id, payload=None):
-        self.id = message_id 
+        self.id = message_id
         self.payload = payload
 
     def __str__(self):
         return 'PeerMessage {}, payload = {}'.format(self.id, self.payload)
 
-    def serialize(self) -> bytes:
-        if self.id == self.Id.KEEP_ALIVE:
-            return bytearray(MESSAGE_LENGTH_SIZE)
-
-        message_length = 1 if not self.payload else 1 + len(self.payload) 
-        ret = bytearray()
-        ret.extend(message_length.to_bytes(self.MESSAGE_LENGTH_SIZE, byteorder='big'))
-        ret.extend(self.id.value.to_bytes(1, byteorder='big'))
-        if self.payload is not None and len(self.payload) > 0:
-            ret.extend(self.payload)
-        return bytes(ret)
-
-    @classmethod
-    def new_request(cls, piece_index, begin, length):
-        payload = bytearray()
-        payload.extend(piece_index.to_bytes(4, byteorder='big'))
-        payload.extend(begin.to_bytes(4, byteorder='big'))
-        payload.extend(length.to_bytes(4, byteorder='big'))
-        return cls(cls.Id.REQUEST, payload=payload)
-    
     @classmethod
     def is_state_message(cls, message_id):
         return message_id in [cls.Id.CHOKE, cls.Id.UNCHOKE, cls.Id.INTERESTED, cls.Id.NOT_INTERESTED]
@@ -151,26 +136,60 @@ class PeerMessage:
         valid_values = [t.value for t in cls.Id]
         return message_id in valid_values
 
+    def serialize(self) -> bytes:
+        if self.id == self.Id.KEEP_ALIVE:
+            return bytearray(MESSAGE_LENGTH_SIZE)
+
+        ret = bytearray()
+
+        message_length = 1 if not self.payload else 1 + len(self.payload)
+        ret.extend(message_length.to_bytes(self.MESSAGE_LENGTH_SIZE, byteorder='big'))
+        ret.extend(self.id.value.to_bytes(1, byteorder='big'))
+        if self.payload:
+            ret.extend(self.payload)
+        return bytes(ret)
+
     @classmethod
-    def receive_from_socket(cls, peer_socket):
-        """Parses a message from the given socket, returns a PeerMessage object"""
-        message_length_bytes = read_from_socket_checked(peer_socket, cls.MESSAGE_LENGTH_SIZE)
+    def new_request(cls, piece_index, begin, length):
+        payload = bytearray()
+        payload.extend(piece_index.to_bytes(4, byteorder='big'))
+        payload.extend(begin.to_bytes(4, byteorder='big'))
+        payload.extend(length.to_bytes(4, byteorder='big'))
+        return cls(self.Id.REQUEST, payload=payload)
+
+    @classmethod
+    def from_ring_buffer(cls, buf):
+        """Parses a PeerMessage from a ring_buffer
+
+        A full message including the payload must be in buf.
+
+        Returns None if there isn't enough data in the buffer
+        """
+        if len(buf) < cls.MESSAGE_LENGTH_SIZE:
+            return None
+        
+        message_length_bytes = buf.peek(cls.MESSAGE_LENGTH_SIZE)
         message_length = int.from_bytes(message_length_bytes, byteorder='big')
 
         if message_length == 0:
-            return cls(cls.Id.KEEP_ALIVE, None)
+            buf.remove(cls.MESSAGE_LENGTH_SIZE)
+            return cls(cls.Id.KEEP_ALIVE, 0)
+        
+        if len(buf) < message_length:
+            # Incomplete header
+            # Don't remove anything from the buffer, wait for next byte to get a full header
+            return None
 
-        message_id = read_from_socket_checked(peer_socket, 1)[0]
-
-        if not cls.is_valid_message_id(message_id):
-            raise ValueError('Unknown message id {}'.format(message_id))
-
-        if message_length == 1:
-            return cls(cls.Id(message_id), None)
+        # Have a full header (length + message id)
+        buf.remove(cls.MESSAGE_LENGTH_SIZE)
+        message_id = cls.Id(buf.read(1)[0])
 
         payload_length = message_length - 1
-        payload_bytes = read_from_socket_checked(peer_socket, payload_length)
-        return cls(cls.Id(message_id), payload_bytes)
+        assert(len(buf) >= payload_length)
+
+        payload = buf.read(payload_length)
+        return cls(message_id, payload)
+    
     
 class Bitfield:
     def __init__(self, bitfield_bytes):
@@ -252,7 +271,16 @@ class PeerConnection:
     # Timeout time in seconds in case a peer fails to connect
     CONNECTION_TIMEOUT_S: int = 5
     MAX_QUEUED_REQUESTS: int = 10
+    BUFFER_PADDING: int = 1024
 
+    class State(enum.Enum):
+        INIT_HANDSHAKE = 0
+        INIT_BITFIELD = 1
+        DOWNLOADING = 2
+        IDLE = 3
+        PAUSED = 4
+        CANCEL = 5
+        DISCONNECTED = 6
 
     def __init__(self, peer_info: Dict, info_hash: bytearray, piece_length):
         self.peer_info = peer_info
@@ -265,10 +293,12 @@ class PeerConnection:
         self.peer_id = None
 
         self.num_queued_requests = 0
+        self.buffer = ring_buffer.RingBuffer(PieceDownload.BLOCK_SIZE_BYTES + self.BUFFER_PADDING)
 
         self.available_pieces = None
+        self.state: self.State = self.State.DISCONNECTED 
+        self.download_state = None
 
-        self.buffer = bytearray()
 
     def __str__(self):
         return "PeerConnection on IP = {}:{} for hash {}".format(self.peer_info['ip'],
@@ -276,47 +306,36 @@ class PeerConnection:
 
     def initialize_connection(self) -> bool:
         """
-        By the end of this function, we should know whether or not we can start downloading from a
-        peer.
+        Begin handshaking sequence, change state to initialized
         """
         # TODO how to track state of connection if this fails?
+        assert(self.state == self.State.DISCONNECTED)
         print('{} initializing connection'.format(str(self)))
 
         self.socket.settimeout(self.CONNECTION_TIMEOUT_S)
         self.socket.connect((self.peer_info['ip'], self.peer_info['port']))
 
-        handshake = PeerHandshake(consts.PEER_ID, self.info_hash)
+        # Get to initializing state once connection succeeds
+        self.state = self.State.INIT_HANDSHAKE
 
+        handshake = PeerHandshake(consts.PEER_ID, self.info_hash)
         self.socket.send(handshake.serialize())
 
-        received_handshake = self.socket.recv(PeerHandshake.HANDSHAKE_SIZE)
-        if not self.validate_handshake(received_handshake):
-            print('{} could not validate handshake'.format(str(self)))
-            return False
+   
+    def validate_handshake(self) -> bool:
+        if len(self.buffer) < PeerHandshake.HANDSHAKE_SIZE:
+            return
 
-        # receive one message, hope it is bitfield message
-        message = PeerMessage.receive_from_socket(self.socket)
-        if message.id != PeerMessage.Id.BITFIELD:
-            print('{} did not receive expected bitfield message'.format(str(self)))
-            return False
-
-        self.handle_bitfield(message.payload)
-        assert(self.available_pieces is not None)
-
-        self.socket.settimeout(None)
-        print('{} Successfully connected'.format(str(self)))
-
-        # Just send that we're interested to all peers
-        self.socket.send(PeerMessage(PeerMessage.Id.INTERESTED, None).serialize())
-        return True
-    
-    def validate_handshake(self, received: bytes) -> bool:
+        received = self.buffer.read(PeerHandshake.HANDSHAKE_SIZE)
         handshake = PeerHandshake.deserialize(received)
         if handshake.info_hash != self.info_hash:
-            return False
+            print("{} recevied bad handshake".format(str(self)))
+            self.state = self.State.DISCONNECTED
+            return
         
         self.peer_id = handshake.peer_id
-        return True
+        self.state = self.State.INIT_BITFIELD
+        self.socket.settimeout(None)
 
     def peer_has_piece(self, index):
         if not self.available_pieces:
@@ -348,33 +367,6 @@ class PeerConnection:
         download_state.handle_block_response(payload)
         self.num_queued_requests -= 1
     
-    def download_piece(self, piece_index):
-        """Runs loop to download a full piece from the peer.
-
-        Assumes that a valid bitfield has been received
-        """
-
-        assert(self.peer_has_piece(piece_index))
-
-        download_state = PieceDownload(piece_index, self.piece_length)
-
-        # in case we aren't choked, send some requests to start
-        while not download_state.all_blocks_received():
-            if not self.choked:
-                self.send_block_requests(download_state)
-
-            message = PeerMessage.receive_from_socket(self.socket)
-            if PeerMessage.is_state_message(message.id):
-                self.handle_state_message(message.id)
-            elif message.id == PeerMessage.Id.BITFIELD:
-                self.handle_bitfield(message.payload)
-            elif message.id == PeerMessage.Id.HAVE:
-                self.handle_have(message.payload)
-            elif message.id == PeerMessage.Id.PIECE:
-                self.handle_piece(message.payload, download_state)
-
-        return download_state.piece_bytes 
-
     def send_block_requests(self, download_state):
         while self.num_queued_requests < self.MAX_QUEUED_REQUESTS:
             if not download_state.has_more_blocks_to_request():
@@ -382,7 +374,100 @@ class PeerConnection:
             next_request = download_state.get_next_block_request()
             self.socket.send(next_request.serialize())
             self.num_queued_requests += 1
-    
+
+    def start_piece_download(self, piece_index):
+        assert(self.state == self.State.IDLE)
+        assert(self.peer_has_piece(piece_index))
+
+        self.download_state = PieceDownload(piece_index, self.piece_length)
+        if not self.choked:
+            self.send_block_requests(self.download_state)
+
+    def append_to_buffer(self, data):
+        self.buffer.write(data)
+
+    def get_piece_bytes(self):
+        if not self.is_download_completed():
+            print('Error: can\'t get piece bytes if download not complete')
+            return None
+        return self.download_state.piece_bytes
+
+    def set_disconnected(self):
+        self.state = self.State.DISCONNECTED
+
+    def set_paused(self):
+        self.state = self.State.PAUSED
+
+    def set_cancelled(self):
+        # TODO send cancel command, or maybe just disconnect?
+        # not sure how to disconnect from socket gracefully
+        # TODO close socket here?
+        self.socket.close()
+        self.buffer.clear()
+        self.state = self.State.DISCONNECTED
+
+    def is_download_completed(self):
+        if not self.download_state:
+            print('Err: no initialized download')
+            return False
+
+        return self.state == self.State.IDLE and self.download_state.all_blocks_received()
+
+    def run_init_states(self):
+        if self.state == self.State.INIT_HANDSHAKE:
+            self.validate_handshake()
+        elif self.state == self.State.INIT_BITFIELD:
+            message = PeerMessage.from_ring_buffer(self.buffer)
+            if not message:
+                return
+
+            if message.id != PeerMessage.Id.BITFIELD:
+                print('Error! {} expected bitfield msg but got {}'.format(str(self), message.id))
+                return
+            
+            print('Connection got bitfield!')
+            self.handle_bitfield(message.payload)
+            self.state = self.State.IDLE
+            self.socket.send(PeerMessage(PeerMessage.Id.INTERESTED, None).serialize())
+
+
+    def handle_messages_from_buffer(handle_piece_message):
+        if self.state == self.State.DISCONNECTED:
+            return
+
+        # Parse all messages from buffer regardless of state
+        # Except Piece messages, which are only handled in downloading state
+        # and should be thrown away otherwise
+        while len(self.buffer) > 0:
+            message = PeerMessage.from_ring_buffer(self.buffer)
+            if message is None:
+                break
+
+            if PeerMessage.is_state_message(message.id):
+                self.handle_state_message(message.id)
+            elif message.id == PeerMessage.Id.BITFIELD:
+                self.handle_bitfield(message.payload)
+            elif message.id == PeerMessage.Id.HAVE:
+                self.handle_have(message.payload)
+            elif message.id == PeerMessage.Id.PIECE and handle_piece_message:
+                self.handle_piece(message.payload, download_state)
+
+    def run_download_state(self):
+        assert(self.state == self.State.DOWNLOADING)
+        if not self.choked:
+            self.send_block_requests()
+        self.handle_messages_from_buffer(True)
+
+    def run_pause_state(self, send_keep_alive=False):
+        assert(self.state == self.State.PAUSED)
+        self.handle_messages_from_buffer(False)
+        if send_keep_alive:
+            self.socket.send(PeerMessage(PeerMessage.Id.KEEP_ALIVE).serialize())
+
+    def run_idle_state(self):
+        assert(self.state == self.State.IDLE)
+        self.handle_messages_from_buffer(False)
+
     
 if __name__ == '__main__':
     metainfo: Dict = tracker.decode_torrent_file('torrent-files/ubuntu.iso.torrent')
@@ -395,13 +480,19 @@ if __name__ == '__main__':
 
     print('piece length: {}'.format(metainfo['info']['piece length']))
     connection = PeerConnection(peer_info, info_hash, metainfo['info']['piece length'])
-    if not connection.initialize_connection():
-        print('bad could not initialize connection')
-        sys.exit(1)
+    connection.initialize_connection()
 
-    for i in range(100000):
-        if connection.peer_has_piece(i):
-            print('REQUESTING PIECE {}'.format(i))
-            piece_bytes = connection.download_piece(i)
+    while True:
+        recv = connection.socket.recv(4096)
+        if len(recv) == 0:
+            print('Failed to initialize!')
+            connection.set_disconnected()
+            sys.exit(1)
+        connection.append_to_buffer(recv)
+        connection.run_init_states()
+        if connection.state != PeerConnection.State.INIT_HANDSHAKE and connection.state != PeerConnection.State.INIT_BITFIELD:
             break
+
+    print('Initialized connection!')
+    print(connection.state)
 
---
+++ backend/tourint/torrent_protocol/ring_buffer.py
@@ -0,0 +1,62 @@
+class RingBuffer:
+    def __init__(self, capacity):
+        assert(capacity > 0)
+        self.buffer = bytearray(capacity)
+        self.read_index = 0
+        self.count = 0
+        self.capacity = capacity
+
+    def __len__(self):
+        return self.count
+
+    def peek(self, num_bytes):
+        ret = bytearray()
+        if num_bytes > self.count:
+            raise ValueError(
+                'Tried to peek too many bytes from RingBuffer: buffer has {}, requested {}'
+                .format(self.count, num_bytes)
+            )
+
+        read_start = self.read_index 
+        read_end = (self.read_index + num_bytes) % self.capacity
+        if read_end < read_start:
+            ret.extend(self.buffer[read_start:self.capacity])
+            ret.extend(self.buffer[:read_end])
+        else:
+            ret.extend(self.buffer[read_start:read_end])
+
+        return ret
+
+    def remove(self, num_bytes):
+        self.read_index = (self.read_index + num_bytes) % self.capacity 
+        self.count -= num_bytes
+
+    def read(self, num_bytes):
+        ret = self.peek(num_bytes)
+        self.remove(num_bytes)
+        return ret
+
+    def write(self, data):
+        if len(data) + self.count > self.capacity:
+            raise ValueError(
+                'Tried to write too many bytes to RingBuffer. ' +
+                'Capacity = {}, Count = {}. data size = {}'
+                .format(self.capacity, self.count, len(data))
+            )
+
+        write_start = (self.read_index + self.count) % self.capacity
+        write_end = (write_start + len(data)) % self.capacity
+
+        if write_end < write_start:
+            num_before_end = self.capacity - write_start
+            self.buffer[write_start:self.capacity] = data[:num_before_end]
+            self.buffer[:write_end] = data[num_before_end:]
+        else:
+            self.buffer[write_start:write_end] = data
+
+        self.count += len(data)
+
+    def clear(self):
+        self.count = 0
+        self.read_index = 0
+
